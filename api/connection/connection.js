81af421234c214552666d0bf9153c6dd818b800e
23rc <23rcroberto@gmail.com>
2023-08-18 18:39:46 -0600

    Backend

---
+++ api/connection/connection.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql2');
+
+const mysqlConnection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'workbench',
+  database: 'LechonSolutionDB',
+  port:3308
+ 
+});
+
+mysqlConnection.connect( err => {
+  if(err){
+    console.log('La base de datos ha presentado un erro!!: ', err);
+    return;
+  }else{
+    console.log('Base de datos conectado exitosamente!!');
+  }
+});
+
+module.exports = mysqlConnection;
