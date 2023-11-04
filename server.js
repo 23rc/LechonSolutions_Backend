105277ed81afd073e431c7b4d005f3ff90eb3a8d
RcrobertoC23 <ryoxonc1@miumg.edu.gt>
2023-11-03 22:32:55 -0600

    Cuarto commit

--- server.js
+++ server.js
@@ -2,6 +2,27 @@
 const http = require('http');
 const app = require('./app');
 const sequelize = require('./config/sequelize');
+const Barraco =require ('./models/Barraco')
+const CamadaLechones =require ('./models/CamadaLechones')
+const Cerdas =require ('./models/Cerdas')
+const Cliente =require ('./models/Cliente')
+const Compras =require ('./models/Compras')
+const ControlCerda =require ('./models/ControlCerda')
+const ControlParto=require ('./models/ControlParto')
+const Destete=require ('./models/Destete')
+const InfoParto=require ('./models/InfoParto')
+const Pacha=require ('./models/Pacha')
+const Parto=require ('./models/Parto')
+const Permiso=require ('./models/Permiso')
+const Producto=require ('./models/Producto')
+const Proveedor=require ('./models/Proveedor')
+const ResumenParto=require ('./models/ResumenParto')
+const Sesion=require ('./models/Sesion')
+const Temperatura=require ('./models/Temperatura')
+const Tratamiento=require ('./models/Tratamiento')
+const User=require ('./models/User')
+const Ventas=require ('./models/Ventas')
+
 
 const port = process.env.PORT || 3000;
 
